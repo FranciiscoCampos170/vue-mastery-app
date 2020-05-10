8adec69e3b59b6d6d2b51f5eb07323573bdc4791
Francisco Campos <franciiscocampos170@gmail.com>
2020-05-10 11:23:51 +0100

    listei os detalhes do produto

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './images/vmSocks-green-onWhite.jpg'
+        image: './images/vmSocks-green-onWhite.jpg',
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"]
     }
 });
