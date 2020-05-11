28264443b1dab58ca1f5258a3e0b83442fb57163
Francisco Campos <franciiscocampos170@gmail.com>
2020-05-11 10:06:48 +0100

    adicionei metodos para o carrinho

--- main.js
+++ main.js
@@ -4,6 +4,27 @@ var app = new Vue({
         product: 'Socks',
         image: './images/vmSocks-green-onWhite.jpg',
         inStock: true,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        cart: 0,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: './images/vmSocks-green-onWhite.jpg',
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: './images/vmSocks-blue-onWhite.jpg',
+            }
+        ],
+    },
+    methods: {
+        addToCart() {
+            this.cart  += 1
+        },
+        updateProduct(variantImage){
+             this.image = variantImage
+        }
     }
 });
