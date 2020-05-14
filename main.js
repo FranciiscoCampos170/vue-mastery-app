c4e935779014e93dc9ca631301e4067be26cb928
Francisco Campos <franciiscocampos170@gmail.com>
2020-05-14 19:46:32 +0100

    adicionei template para o produto

--- main.js
+++ main.js
@@ -1,30 +1,94 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        image: './images/vmSocks-green-onWhite.jpg',
-        inStock: false,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        cart: 0,
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantImage: './images/vmSocks-green-onWhite.jpg',
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: './images/vmSocks-blue-onWhite.jpg',
-            }
-        ],
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true,
+
+        }
+    },
+    template:`
+    <div class="product">
+        <div class="product-image">
+        <img :src="image" alt = "">
+        </div>
+        <div class="product-info">
+        <h1> {{product}} </h1> 
+        <p v-if="inStock"> In Stock </p> 
+        <p v-else>Out of Stock </p>
+        <p>Shipping: {{ Shipping }}</p>
+        <ul>
+            <li v-for="detail in details"> {{detail}} </li>
+        </ul>
+        <div v-for="(variant, index) in variants"
+        :key="variant.variantId"
+        class="color-box"
+        :style="{backgroundColor: variant.variantColor}"
+        @mouseover="updateProduct(index)" >
+        </div>
+
+        <button @click="addToCart" 
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock}" >
+        Add to Cart </button>
+
+        <div class = "cart" >
+        <p> Cart({{cart}}) </p> 
+        </div>
+
+        </div>
+        </div>
+        `,
+    data() {
+        return {
+            product: 'Socks',
+            selectedVariant: 0,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            cart: 0,
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantImage: './images/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantImage: './images/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            
+        }
     },
     methods: {
         addToCart() {
-            this.cart  += 1
+            this.cart += 1
         },
-        updateProduct(variantImage){
-             this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
         }
+    },
+    computed: {
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        Shipping() {
+            if(this.premium){
+                return "Free"
+            }
+            return 2.99
+        }
+    }
+
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: false
     }
 });
